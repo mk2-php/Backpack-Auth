4ea2b10ed6ea6083183652c7ea07581d8c145ae9
nakatsuji masato <nakatsuji@teastalk.jp>
2021-07-01 13:08:11 +0900

    7/1 Add Readme.md

--- src/AuthBackpack.php
+++ src/AuthBackpack.php
@@ -1,5 +1,22 @@
 <?php
 
+/**
+ * ===================================================
+ * 
+ * PHP Framework "Mk2"
+ *
+ * AuthBackpack
+ * 
+ * Backpack class base file.
+ * 
+ * URL : https://www.mk2-php.com/
+ * 
+ * Copylight : Nakajima-Satoru 2021.
+ *           : Sakaguchiya Co. Ltd. (https://www.teastalk.jp/)
+ * 
+ * ===================================================
+ */
+
 namespace mk2\backpack_auth;
 
 use Mk2\Libraries\Backpack;
